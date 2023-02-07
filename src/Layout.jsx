a1a77221fd153cbef417d7ad915aa164b97d7ef0
bhimathakur@gmail.com <bhimathakur@gmail.com>
2023-02-07 14:51:55 +0530

    Initial commit

---
+++ src/Layout.jsx
@@ -0,0 +1,74 @@
+import { Outlet, NavLink } from "react-router-dom";
+import Onboarding from "./Onboarding";
+import ValidateAddress from "./ValidateAddress";
+import ValidateCard from "./ValidateCard";
+
+const Layout = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item ">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/register"
+              >
+                Registration
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/address"
+              >
+                Validate Address
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/card"
+              >
+                Validate Card
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/contact"
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Outlet />
+      
+    </>
+  );
+};
+
+export default Layout;
